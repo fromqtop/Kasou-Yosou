44687584e6a618ef5c640f9b03e2f88a207cf064
ichi3270 <fromqtop@gmail.com>
2026-01-25 06:26:10 +0900

    レイアウト微修正

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -14,7 +14,7 @@ const Layout: React.FC<Props> = ({ user, refetchUser }) => {
       <div className="min-h-screen bg-zinc-900 text-white">
         <Header user={user} />
 
-        <main className="px-4">
+        <main>
           <Outlet context={{ user, refetchUser }} />
         </main>
       </div>
